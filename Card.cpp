b55faab2606d9d26c742b9f2649dd01613073b31
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-05 00:25:39 +1030

    First pass logic for Card.cpp added

---
+++ Card.cpp
@@ -0,0 +1,30 @@
+#include "Card.h"
+
+// Constructor implementation
+Card::Card(CardType type, int pointValue) 
+	: _type(type), _pointValue(pointValue) 
+{
+}
+
+// Getter for CardType
+// To avoid copying and modification, a const reference to the Card's type is returned
+const CardType& Card::type() const 
+{
+	return _type;
+}
+
+
+// Getter for pointValue
+// Simple value return
+int Card::pointValue() const
+{
+	return _pointValue;
+}
+
+// Default implementation of willAddToBank()
+void Card::willAddToBank(Game& game, Player& player)
+{
+	// Default action is no Banking effect
+	// Intentionally blank
+	// Derived classes can override this method to define special behaviour when added to a Player's Bank
+}
