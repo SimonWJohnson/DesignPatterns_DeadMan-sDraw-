dfbfa36166cc2fee509eaefa6a174a7b7e3c9249
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-05 00:59:45 +1030

    First pass logic for Player added to Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,45 @@
 #pragma once
+
+#include <string>
+#include "Card.h"
+
+/*
+Responsibilities:
+- manage play area
+- detect bust
+- manage bank
+- calculate score
+- print collections
+*/
+
+// Forward declaration of Game
+// Tells the compiler Game exists without including Game.h here
+class Game;
+
+class Player 
+{
+public:
+	// Constructor
+	// Creates a Player object with a given name
+	Player(const std::string& name);
+
+	// Destructor
+	// Clean up dynamically allocated Card pointers 
+	~Player();
+
+	// Getter for Player name
+	// Returns Player name as a const reference to avoid copying and modification
+	const std::string& name() const;
+
+private:
+	// Store the Player's name
+	std::string _name;
+
+	// Store Cards the Player has played this round but not yet banked or discarded
+	CardCollection _playArea;
+
+	// Store Cards the Player has successfully banked
+	// Contributed to Score
+	CardCollection _bank;
+
+};
