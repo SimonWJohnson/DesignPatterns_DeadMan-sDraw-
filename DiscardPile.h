19fb78a4dcfd8f9c71ae1f31304010c832bde8ab
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-06 17:46:32 +0930

    Added implementation logic to DiscardPile.h

--- DiscardPile.h
+++ DiscardPile.h
@@ -1,1 +1,41 @@
 #pragma once
+
+
+#include "Card.h"
+
+class DiscardPile 
+{
+public:
+	// Constructor
+	DiscardPile();
+
+	// Deconstructor
+	~DiscardPile();
+
+	// Add a Card to the DiscardPile
+	void add(Card* card);
+
+	// Draw a Card from the top of the DiscardPile (end of the vector)
+	Card* drawTop();
+
+	// Check if DiscardPile is empty
+	bool empty() const;
+
+	// Check how many Cards are in the DiscardPile
+	int size() const;
+
+	// Designate how many Cards to draw from the DiscardPile
+	// Returns a collection of those Cards
+	CardCollection drawUpTo(int count);
+
+	// Return a reference to the _cards collection
+	// Allows modification
+	CardCollection& cards();
+
+	// Returns a read only reference to _cards
+	// Prevents modification
+	const CardCollection& cards() const;
+
+private:
+	CardCollection _cards;
+};
