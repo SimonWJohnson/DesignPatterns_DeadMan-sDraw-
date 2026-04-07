93a89ef4259d9fe2265a13c3ebc02ba9e171efbf
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-07 12:32:21 +0930

    Added psuedocode to play() function

--- OracleCard.cpp
+++ OracleCard.cpp
@@ -14,7 +14,29 @@ std::string OracleCard::str() const
 
 void OracleCard::play(Game& game, Player& player)
 {
+	// Psuedocode
+	// std::cout << ability message
 	std::cout << "Peek at the top card of the deck before choosing whether to draw" << std::endl;
+	// Peek top Card of Deck - game.deck().peekTop()
+	// IF _deck enpty()
+	//		std::cout << "No cards in deck" << std::endl;
+	//		return
+	// Print topCard for _player to see
+	// std::cout << ""The Oracle sees a <topCard>" << std::endl;
+	// Prompt _player to choose to draw the Card
+	// Read / validate input
+	// IF input = "n"
+	//		no action
+	//		return
+	// IF input = "y"
+	//		draw Card from _deck
+	//		Print "<_player> draws a <topCard>"
+	//		call player.playCard(drawnCard, game)
+	//		IF bust:
+	//			std:cout bust message
+	//			move Cards in _playArea to _discardPile
+	// ELSE play continues
+	//		std::cout << "Play continues" << std::endl; 
 }
 
 void OracleCard::willAddToBank(Game& game, Player& player)
