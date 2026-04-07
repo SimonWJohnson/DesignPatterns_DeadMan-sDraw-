f0b51fac842812a20224027cd422b99032e75b29
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-07 13:05:37 +0930

    Added psuedocode for play() function

--- KrakenCard.cpp
+++ KrakenCard.cpp
@@ -14,7 +14,31 @@ std::string KrakenCard::str() const
 
 void KrakenCard::play(Game& game, Player& player)
 {
+	// Psuedocode
+	// Print Card special ability
 	std::cout << "Must draw and play three cards consecutively" << std::endl;
+	// Repeat 3 forced draws using FOR loop
+	//		IF _deck empty(), no more draws can occur
+	//		std::cout << "" << std::endl;
+	//		return
+	//		Draw next Card from _deck
+	//		Validate Card for nullptr (end of _deck)
+	//			std:cout message for empty _deck
+	//			return
+	// Display drawn Card to _currentPlayer - std::cout <<
+	// Call player.playCard()
+	//		drawn Card tranferred to _playArea
+	//		_player checked for bust
+	//		IF bust:
+	//			print bust message
+	//			Cards removed from _playArea, transferred to _discardPile
+	//			_currentPlayer turn ends, switch to _otherPlayer
+	//		ELSE
+	//			All forced Draws complete without bust
+	//			Play resumes as normal for _currentPlayer
+	//	Print message to signify end of Kraken effect
+	//		std::cout << "Kraken effect complete" << std::endl;
+
 }
 
 void KrakenCard::willAddToBank(Game& game, Player& player)
