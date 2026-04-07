9d5f56fa88e2f6539bf71464fed57859464a1664
SimonWJohnson <simonwjohnson@gmail.com>
2026-04-07 10:33:24 +0930

    Added psuedocode to play() method

--- CannonCard.cpp
+++ CannonCard.cpp
@@ -14,7 +14,21 @@ std::string CannonCard::str() const
 
 void CannonCard::play(Game& game, Player& player)
 {
+	// Psuedocode
+	// Print Card ability
 	std::cout << "Discard the top card (i.e. the highest value) of any suit from the other player's Bank to the Discard Pile" << std::endl;
+	// Get reference to other Player / game.otherPlayer()
+	// IF _otherPLayer's _bank.empty()
+	// std::cout << "No cards in _otherPlayer's _bank" Play continues (return)
+	// Display _otherPLayer's _bank so _currentPLayer can choose a suit
+	// std::cout
+	// Prompt _currentPlayer to select a suit 
+	// Determine which suit has been chosen
+	// Remove highest pointValue Card from selected suit from _otherPlayer's _bank
+	// IF no Card is found
+	// std::cout no card found message, return
+	// Add removed Card to _discardPile
+	// std::cout which Card was discarded
 }
 
 void CannonCard::willAddToBank(Game& game, Player& player)
